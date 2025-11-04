b88c3564cc6621d0e9a509a681c3c8b2c184c205
juozapas_laptop <juozapas@prorega.lt>
2025-11-04 08:32:45 +0200

    Knygų skolinimų posistemės prototipas baigtas

---
+++ InformacinesSistemos/Controllers/LendingController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InformacinesSistemos.Controllers
+{
+    public class LendingController : Controller
+    {
+        public IActionResult LendBook() => View();
+
+        public IActionResult ReturnBook() => View();
+    }
+}
---
+++ InformacinesSistemos/Controllers/RecommendationController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InformacinesSistemos.Controllers
+{
+    public class RecommendationController : Controller
+    {
+        public IActionResult ViewRecommendation() => View();
+    }
+}
